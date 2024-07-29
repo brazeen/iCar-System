55697da13009f8198dd43326d48d285442f847a1
brazeen <brandonkohzh@gmail.com>
2024-07-29 23:15:06 +0800

    made Admin and User class files

---
+++ iCar System/Admin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCar_System
+{
+    public class Admin
+    {
+        private int adminID;
+
+        public int AdminID { get; set; }
+
+        private string adminPassword;
+
+        public string AdminPassword { get; set; }
+
+        private string name;
+
+        public string Name { get; set; }
+
+        private List<User> authenticatedUsersList;
+
+        //constructor
+        public Admin() { }
+
+        public Admin(int aid, string apw, string n)
+        {
+            AdminID = aid;
+            AdminPassword = apw;
+            Name = n;
+        }
+
+    }
+}
---
+++ iCar System/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCar_System
+{
+    public class User
+    {
+        private int userID;
+
+        public int UserID { get; set; }
+
+        private string name;
+
+        public string Name { get; set; }
+
+        private string contactNumber;
+
+        public string ContactNumber { get; set; }
+
+        private DateTime dateOfBirth;
+
+        public DateTime DateOfBirth { get; set; }
+
+        private string emailAddress;
+
+        public string EmailAddress { get; set; }
+
+        private string homeAddress;
+
+        public string HomeAddress { get; set; }
+
+        private Admin authenticatedAdmin;
+
+        public Admin AuthenticatedAdmin { get; set; }
+
+        //constructor
+        public User() { }
+
+        public User(int uid, string n, string cn, DateTime dob, string ea, string ha, Admin aa)
+        {
+            UserID = uid;
+            Name = n;
+            ContactNumber = cn;
+            DateOfBirth = dob;
+            EmailAddress = ea;
+            HomeAddress = ha;
+            AuthenticatedAdmin = aa;
+        }
+    }
+}
