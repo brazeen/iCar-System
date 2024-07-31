07029ed16935f736b721f41ac38ff519f4d36d86
brazeen <brandonkohzh@gmail.com>
2024-07-31 22:32:14 +0800

    insurance class

---
+++ iCar System/Insurance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCar_System
+{
+    public class Insurance
+    {
+        private int insuranceNo; // should it be a string ah
+
+        public int InsuranceNo { get; set; }
+
+        private string company;
+
+        public string Company { get; set; }
+
+        private DateTime expirationDate;
+
+        public DateTime ExpirationDate { get; set; }
+
+        private string coverage;
+
+        public string Coverage { get; set; }
+
+        private Car insuredCar;
+
+        public Car InsuredCar { get; set; }
+
+        //constructor
+        public Insurance() { }
+
+        public Insurance(int ino, string c, DateTime ed, string cvg, Car ic)
+        {
+            InsuranceNo = ino;
+            Company = c;
+            ExpirationDate = ed;
+            Coverage = cvg;
+            InsuredCar = ic;
+        }
+    }
+}
