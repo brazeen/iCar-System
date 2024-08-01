bd6d9ab36be880a053ef238e4c82df8180574727
brazeenNP <s10255790@connect.np.edu.sg>
2024-08-01 22:09:54 +0800

    monthlypayment class added

---
+++ iCar System/MonthlyPayment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCar_System
+{
+	public class MonthlyPayment
+	{
+		private int paymentID;
+
+		public int PaymentID { get; set; }
+
+		//meow meow mmeow meow
+
+		private DateTime paymentDate;
+
+		public DateTime PaymentDate { get; set; }
+
+		private double paymentAmount;
+
+		public double PaymentAmount { get; set; }
+
+		private string paymentMethod;
+
+		public string PaymentMethod { get; set; }
+
+		private List<Booking> bookingList;
+
+		//constructor
+		public MonthlyPayment() { }
+
+		public MonthlyPayment(int pid, DateTime pd, double pa, string pm)
+		{
+			PaymentID = pid;
+			PaymentDate = pd;
+			PaymentAmount = pa;
+			PaymentMethod = pm;
+			bookingList = new List<Booking>();
+		}
+	}
+}
