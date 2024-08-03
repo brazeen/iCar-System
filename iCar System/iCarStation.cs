201b05ca058046fa75b4ce561c2d880acd8e15ed
donovan1025 <S10257381@connect.np.edu.sg>
2024-08-04 00:29:25 +0800

    Added iCar station class

---
+++ iCar System/iCarStation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCar_System
+{
+    class iCarStation
+    {
+        private int stationID;
+
+        public int StationID { get { return stationID; } set { stationID = value; } }
+
+        private int postalCode;
+
+        public int PostalCode { get { return postalCode; } set { postalCode = value; } }
+
+        public iCarStation() { }
+
+        public iCarStation(int sID, int pc)
+        {
+            stationID = sID;
+            postalCode = pc;
+        }
+    }
+}
