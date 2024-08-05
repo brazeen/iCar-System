b527b7bf2a9d8749df659e69c0ef1b7bb03c7948
brazeen <brandonkohzh@gmail.com>
2024-08-05 14:47:01 +0800

    changed to match the new changes in booking class

--- iCar System/Brandon.cs
+++ iCar System/Brandon.cs
@@ -10,7 +10,33 @@ namespace iCar_System
     {
         static void Main(string[] args)
         {
-            //control methods
+
+
+            //test data------------------------------------------------------------------------------------------------------------
+            Renter testrenter = new Renter(1, "Brandon Koh", "99345673", new DateTime(1985, 11, 23), "brandon@gmail.com", "2 Clementi Road");
+            Booking testBooking1 = new Booking(1, new DateTime(2024, 1, 1, 9, 0, 0), new DateTime(2024, 2, 2, 17, 0, 0), new Tuple<string, string>("Deliver", "239085"), new Tuple<string, string>("Deliver", "345676"), 199.99, 100);
+            Booking testBooking2 = new Booking(2, new DateTime(2024, 4, 1, 10, 0, 0), new DateTime(2024, 5, 5, 15, 0, 0), new Tuple<string, string>("Deliver", "665432"), new Tuple<string, string>("Deliver", "170645"), 100.99, 99);
+
+            List<string> photos1 = new List<string>
+            {
+                "photo1.jpg",
+                "photo2.jpg"
+            };
+
+            List<string> photos2 = new List<string>
+            {
+                "photo3.jpg",
+                "photo4.jpg"
+            };
+
+            Car car1 = new Car(1, "Toyota Camry", 2022, 5000, photos1, "Toyota", 29.99);
+            Car car2 = new Car(2, "Honda Civic", 2021, 10000, photos2, "Honda", 24.99);
+            testBooking1.setCar(car1);
+            testBooking2.setCar(car2);
+            testrenter.Bookings.Add(testBooking1);
+            testrenter.Bookings.Add(testBooking2);
+
+            //control methods------------------------------------------------------------------------------------------------------
             bool validateReview(int rating, string description)
             {
                 if ((rating < 1 || rating > 5) || (description.Length > 200))
@@ -35,124 +61,120 @@ namespace iCar_System
                 }
             }
 
-            //test data
-            Renter loggedInRenter = new Renter(1, "Brandon Koh", "99345673", new DateTime(1985, 11, 23), "brandon@gmail.com", "2 Clementi Road");
-            Booking testBooking1 = new Booking(1, new DateTime(2024, 1, 1, 9, 0, 0), new DateTime(2024, 2, 2, 17, 0, 0), "123 Main St", "456 Elm St", 199.99, 100);
-            Booking testBooking2 = new Booking(2, new DateTime(2024, 4, 1, 10, 0, 0), new DateTime(2024, 5, 5, 15, 0, 0), "789 Pine St", "123 Main St", 100.99, 99);
-            List<DateTime> schedule1 = new List<DateTime>
+            List<Booking> getRenterBookingHistory(Renter r)
             {
-                new DateTime(2024, 8, 10, 9, 0, 0),
-                new DateTime(2024, 8, 17, 17, 0, 0)
-            };
+                return r.getBookingHistory();
+            }
 
-            List<DateTime> schedule2 = new List<DateTime>
+            Car selectCarInBooking(List<Booking> bookingHistory)
             {
-                new DateTime(2024, 9, 1, 10, 0, 0),
-                new DateTime(2024, 9, 5, 15, 0, 0)
-            };
+                while (true)
+                {
+                    Console.Write("Select a booking ID to view: ");
+                    int inputbookingid = Convert.ToInt32(Console.ReadLine());
+                    foreach (Booking booking in bookingHistory)
+                    {
+                        if (booking.BookingId == inputbookingid)
+                        {
+                            Car car = booking.getCarInBooking();
+                            return car;
+                        }
+                    }
+                    Console.WriteLine("Booking not found. Please re-input the booking ID.");
+                }
 
-            List<string> photos1 = new List<string>
+            }
+            //UI methods-------------------------------------------------------------------------------------------------------------
+
+            //to simulate finding a logged in renter, return the test renter object 
+            Renter findLoggedInRenter()
             {
-                "photo1.jpg",
-                "photo2.jpg"
-            };
+                return testrenter;
+            }
 
-            List<string> photos2 = new List<string>
+            void displayBooking(Booking booking)
             {
-                "photo3.jpg",
-                "photo4.jpg"
-            };
-
-            Car car1 = new Car(1, "Toyota Camry", 2022, 5000, photos1, "Toyota", 29.99, schedule1);
-            Car car2 = new Car(2, "Honda Civic", 2021, 10000, photos2, "Honda", 24.99, schedule2);
-            testBooking1.setCar(car1);
-            testBooking2.setCar(car2);
-            loggedInRenter.Bookings.Add(testBooking1);
-            loggedInRenter.Bookings.Add(testBooking2);
-
-
-
-
-
-
-
-            //use case start
-            List<Booking> bookingHistory = loggedInRenter.getBookingHistory();
-            string bookingDetails;
-            foreach (Booking booking in bookingHistory)
-            {
-                bookingDetails = $"Booking ID: {booking.BookingId}\n" +
+                string bookingDetails = $"Booking ID: {booking.BookingId}\n" +
                                                 $"Start Date and Time: {booking.StartDateAndTime}\n" +
                                                 $"End Date and Time: {booking.EndDateAndTime}\n" +
-                                                $"Pickup Location: {booking.PickupLocation}\n" +
-                                                $"Return Location: {booking.ReturnLocation}\n" +
-                                                $"Total Cost: ${booking.BookingFee+booking.RoadSideFee}\n\n";
+                                                $"Pickup Location: {booking.PickUpDetails}\n" +
+                                                $"Return Location: {booking.DropOffDetails}\n" +
+                                                $"Total Cost: ${booking.BookingFee + booking.RoadSideFee}\n\n";
 
                 Console.WriteLine(bookingDetails);
             }
 
-            //for the loop to run at least once
-            bool pastReview = true;
-            //variable to track if option is in range (actual use case will be in a GUI so will not have that logic)
-            bool foundbookingid = false; 
-            Car car = new Car();
-            while (pastReview == true)
+            void displayCarDetails(Car car)
             {
-                Console.Write("Select a booking ID to view: ");
-                int inputbookingid = Convert.ToInt32(Console.ReadLine());
-                foreach (Booking booking in bookingHistory)
-                {
-                    if (booking.BookingID == inputbookingid)
-                    {
-                        foundbookingid = true;
-                        car = booking.getCarInBooking();
-                    }
-                }
-                if (car.CarID != null)
-                {
-                    string carDetails = "Car Details:\n" +
+                string carDetails = "Car Details:\n" +
                                                 $"Model: {car.Model}\n" +
                                                 $"Year: {car.Year}\n" +
                                                 $"Make: {car.Make}\n" +
                                                 $"Mileage: {car.Mileage} kilometers\n" +
                                                 $"Rate: ${car.Rate} per hour\n";
-                    Console.WriteLine(carDetails);
-                    Console.WriteLine("1: Make Review, 2: Exit");
-                    Console.Write("Enter option: ");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    bool inrangeoption = false;
-                    while (inrangeoption == false)
+                Console.WriteLine(carDetails);
+            }
+
+            void displayErrorMessage(string error)
+            {
+                Console.WriteLine(error);
+            }
+
+            void displayConfirmationPopup(Review r)
+            {
+                string reviewDetails = "Review Details:\n" +
+                                                $"Rating: {r.Rating}\n" +
+                                                $"Description: {r.Description}\n" +
+                                                "Review created successfully.\n";
+            }
+            //use case start-----------------------------------------------------------------------------------------------------------
+            Renter loggedInRenter = findLoggedInRenter();
+            List<Booking> bookingHistory = getRenterBookingHistory(loggedInRenter);
+            foreach (Booking booking in bookingHistory)
+            {
+                displayBooking(booking);
+            }
+
+            //for the loop to run at least once
+            bool pastReview = true;
+            //car has to be defined first as defining it in the loop would cause the createReview method to have a null car variable
+            Car car = new Car();
+            while (pastReview == true)
+            {
+                car = selectCarInBooking(bookingHistory);
+
+                Console.WriteLine("1: Make Review, 2: Exit");
+                //option and option validation will not be in actual iCar system as it will be a GUI based system)
+                Console.Write("Enter option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                //variable to track if option is in range (actual use case will be in a GUI so will not have that logic)
+                bool inrangeoption = false;
+                while (inrangeoption == false)
+                {
+                    if (option == 1)
                     {
-                        if (option == 1)
+                        inrangeoption = true;
+                        pastReview = hasReviewed(loggedInRenter, car);
+                        if (pastReview)
                         {
-                            inrangeoption = true;
-                            pastReview = hasReviewed(loggedInRenter, car);
-                            if (pastReview)
-                            {
-                                //display error message
-                                Console.WriteLine("A review has already been made for this car.");
-                            }
-                            else
-                            {
-                                pastReview = false;
-                            }
-                        }
-                        else if (option == 2)
-                        {
-                            inrangeoption = true;
-                            return;
+                            //display error message
+                            displayErrorMessage("A review has already been made for this car.");
                         }
                         else
                         {
-                            Console.WriteLine("Invalid option");
+                            pastReview = false;
                         }
                     }
+                    else if (option == 2)
+                    {
+                        inrangeoption = true;
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid option");
+                    }
                 }
-                if (foundbookingid == false)
-                {
-                    Console.WriteLine("Booking ID not found.");
-                }
-
             }
 
             bool valid = false;
@@ -160,6 +182,7 @@ namespace iCar_System
             string description = "";
             while (valid == false)
             {
+                //displayInputFields +inputReview method would work differently in a GUI as both fields can be displayed together without getting input first
                 Console.Write("Enter rating from 1 to 5: ");
                 rating = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Enter description (max 200 characters): ");
@@ -172,13 +195,13 @@ namespace iCar_System
                 }
             }
 
-            //create review
-            Review review = new Review(1, rating, description); // to change later as this ID is hardcode
-            review.setRenter(loggedInRenter);
-            review.setCar(car);
-            loggedInRenter.addReview(review);
-            car.addReview(review);
-            Console.WriteLine("Review created successfully.");
+            //createReview method in sequence diagram is represented by using the constructor to create a new review in line 191
+            Review newReview = new Review(1, rating, description); 
+            newReview.setRenter(loggedInRenter);
+            newReview.setCar(car);
+            loggedInRenter.addReview(newReview);
+            car.addReview(newReview);
+            displayConfirmationPopup(newReview);
         }
     }
 }
